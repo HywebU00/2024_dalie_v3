f6de9b87fe0fefd26fa051bdd1fc75bd5246e7e5
Lize Wu <retniw72@gmail.com>
2024-09-26 10:16:55 +0800

    first commit

--- js/customize.js
+++ js/customize.js
@@ -1,219 +1,82 @@
-// 自行加入的JS請寫在這裡
-$(function () {
-  // 首頁輪播
-  $('.mpSlider').slick({
-    mobileFirst: true,
-    dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    fade: true,
-    lazyLoaded: true,
-    lazyLoad: 'ondemand',
-    ease: 'ease',
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    customPaging: function (slider, i) {
-      var title = $(slider.$slides[i]).find('img').attr('alt').trim();
-      return $('<button type="button" aria-label="' + title + '"/>').text(title);
-    },
-  });
-  // 廣告輪播
-  $('.adSlider').slick({
-    mobileFirst: true,
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrow: true,
-    lazyLoaded: true,
-    lazyLoad: 'ondemand',
-    ease: 'ease',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          arrows: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          arrows: true,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          arrows: true,
-        },
-      },
-    ],
-  });
-  //燈箱slick+lightBox組合
-  $('.cp_slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 1500,
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    focusOnSelect: true,
-    accessibility: true,
-    lazyLoad: 'ondemand',
-    ease: 'ease',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 545,
-        settings: {
-          arrows: true,
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
-  });
-  //
-  $('.cppic_slider').slick({
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 1500,
-    // pauseOnHover: true,
-    // pauseOnFocus: true,
-    // focusOnSelect: true,
-    // accessibility: true,
-    // lazyLoad: 'ondemand',
-    // ease: 'ease',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 545,
-        settings: {
-          arrows: true,
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
-  });
-  // cp_photo
-  $('.Slider-for').on('init reInit afterChange', function (event, slick, currentSlide) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $('.controls').html(i + '/' + slick.slideCount);
-  });
-  $('.Slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    swipe: false,
-    swipeToSlide: false,
-    lazyLoad: 'ondemand',
-    asNavFor: '.Slider-nav',
-    infinite: true,
-  });
-  $('.Slider-nav').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    asNavFor: '.Slider-for',
-    dots: true,
-    arrows: true,
-    lazyLoad: 'ondemand',
-    focusOnSelect: true,
-    infinite: true,
-  });
-
-  // password_toggle
-  var passShow = false;
-  $('.password_toggle').each(function (index, el) {
-    $(this)
-      .find('.btn-icon')
-      .off()
-      .click(function (e) {
-        if (!passShow) {
-          $(this).children('i').removeClass().addClass('i_show');
-          $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
-          passShow = true;
-          // console.log(passShow);
+$(document).ready(function() {
+    // 手機版 search btn switch
+    $('.mobileSearchBtn').click(function() {
+        if ($('.mobileShow').hasClass('act')) {
+            $('.mobileShow').removeClass('act').fadeOut();
         } else {
-          $(this).children('i').removeClass().addClass('i_hide');
-          $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
-          passShow = false;
-          // console.log(passShow);
+            $('.mobileShow').addClass('act').fadeIn();
         }
-        e.preventDefault();
-      });
-  });
-  //sticky sidebar
-  // if ($('.stickySidebar').length > 0) {
-  //   var stickySidebar = new StickySidebar('.stickySidebar', {
-  //     containerSelector: '.main',
-  //     topSpacing: 93,
-  //     bottomSpacing: 0,
-  //     minWidth: 768,
-  //     resizeSensor: true,
-  //   });
-  // }
-});
+    });
+    // 寬度異動時，移除.act
+    $(window).resize(function() {
+        if ($(window).width() > 991) {
+            $('.mobileShow').removeClass('act').hide();
+        }
+    });
 
-// tab功能
-tabFunction({
-  target: '.tabSet',
-  openFirst: false, // 預設先展開所有內容，鍵盤的自動開合功能無效
-  openSwitch: true, // 是否可開合/切換
-  autoClose: true, // 自動關閉其他開啟內容
-  modeSwitch: true, // 預設模式自動切換，尺寸以上tab功能，尺寸以下手風琴功能
-  width: 767, // 尺寸以上tab功能，尺寸以下手風琴功能
-  index: 0, // 預設開啟第幾個
+    function checkHeight() {
+        if ($('.mask p').height() < 400) {
+            $('.mask button.btn').hide();
+            $('.mask:after').hide();  // 請注意這裡 :after 需要特別處理
+        } else {
+            $('.mask button.btn').show();
+            $('.mask:after').show();  // 同上
+        }
+    }
+    // 初次檢查
+    checkHeight();
+    // 當窗口大小變化時重新檢查
+    $(window).resize(function() {
+        checkHeight();
+    });
+
+    // 內容摘要 閱讀全文 btn
+    $('.mask .btn').click(function() {
+        var $mask = $('.mask');
+        var $btn = $(this);
+        if ($mask.hasClass('_show')) {
+            $mask.removeClass('_show');
+            $btn.text('閱讀全文');
+        } else {
+            $mask.addClass('_show');
+            $btn.text('顯示較少內容');
+        }
+    });
+
+    // 社群icon 分享
+    $('.function .share').on('click', function() {
+        if (!$(this).hasClass('act')) {
+            $(this).addClass('act');
+            $(this).siblings('.socialMedia').addClass('Show');
+        } else {
+            $(this).removeClass('act');
+            $(this).siblings('.socialMedia').removeClass('Show');
+        }
+    });
+
+    /*-----------------------------------*/
+    ///////////////置頂go to top////////////
+    /*-----------------------------------*/
+    $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+        $('.scrollToTop').fadeIn(200);
+    } else {
+        $('.scrollToTop').fadeOut(200);
+    }
+    });
+    /*-----------------------------------*/
+    /////click event to scroll to top//////
+    /*-----------------------------------*/
+    $('.scrollToTop')
+        .off()
+        .on('click', function (e) {
+            $('html, body').stop().animate({ scrollTop: 0 }, 400, 'linear');
+            // $('a.goCenter').focus(); //加入這行
+            e.preventDefault();
+        });
+        $('.scrollToTop').keydown(function (e) {
+        $('html, body').stop().animate({ scrollTop: 0 }, 400, 'linear');
+        $('body').find('a.goCenter').focus();
+        e.preventDefault();
+    });
 });
