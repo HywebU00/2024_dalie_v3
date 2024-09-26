9f79c4c8b6e29445ba18bcd2b89a5209e7ac5812
Lize Wu <retniw72@gmail.com>
2024-09-26 13:56:44 +0800

    無障礙快捷鍵JQ 縮排調整

--- js/customize.js
+++ js/customize.js
@@ -60,34 +60,34 @@ $(document).ready(function() {
     $(document).on('keydown', function (e) {
         // alt+S 查詢
         if (e.altKey && e.keyCode == 83) {
-        $('html, body').animate({ scrollTop: 0 }, 200, 'easeOutExpo');
-        $('.search').find('input[type="text"]').focus();
+            $('html, body').animate({ scrollTop: 0 }, 200, 'easeOutExpo');
+            $('.search').find('input[type="text"]').focus();
         }
         // alt+U header
         if (e.altKey && e.keyCode == 85) {
-        $('html, body').animate({ scrollTop: 0 }, 200, 'easeOutExpo');
-        $('header').find('.accesskey').focus();
+            $('html, body').animate({ scrollTop: 0 }, 200, 'easeOutExpo');
+            $('header').find('.accesskey').focus();
         }
         // alt+C 主要內容區
         if (e.altKey && e.keyCode == 67) {
-        $('html, body')
-            .stop(true, true)
-            .animate({ scrollTop: $('.main').find('.accesskey').offset().top - 70 }, 800, 'easeOutExpo');
-        $('.main').find('.accesskey').focus();
+            $('html, body')
+                .stop(true, true)
+                .animate({ scrollTop: $('.main').find('.accesskey').offset().top - 70 }, 800, 'easeOutExpo');
+            $('.main').find('.accesskey').focus();
         }
         // alt+Z footer
         if (e.altKey && e.keyCode == 90) {
-        $('html, body')
+            $('html, body')
             .stop(true, true)
             .animate({ scrollTop: $('footer').find('.accesskey').offset().top }, 800, 'easeOutExpo');
-        $('footer').find('.accesskey').focus();
+            $('footer').find('.accesskey').focus();
         }
 
         if (e.key == 'Escape') {
-        $('.language ul').slideUp();
-        $('.language > a').blur();
-        $('.menu ul li ul').hide();
-        $('.menuArrowDown, .menuArrowUp').remove();
+            $('.language ul').slideUp();
+            $('.language > a').blur();
+            $('.menu ul li ul').hide();
+            $('.menuArrowDown, .menuArrowUp').remove();
         }
     });
 
